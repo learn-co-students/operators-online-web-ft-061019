504a382a4aab4d90256ac4e318719f32a85759e0
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-11 19:47:39 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
 def unsafe?(speed)
-
+  
+speed == 79
+if speed > 60
+  return true
+ end
+if speed < 40
+  return true
+ else 
+   return false
+ end
 end
 
 
 
 def not_safe?(speed)
-	
-end
-	
-
-
+  speed == (79,35)
+  speed > 60 ? true : false
+  speed < 40 ? true : false
+  end
+  
